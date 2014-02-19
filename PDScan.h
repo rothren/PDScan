b9c5103f416e263d0e89a73039353f620fa1e98d
rothren <roth.rene@gmail.com>
2014-02-19 13:54:31 +0100

    START
    Project created, attributes & ignore liste updated.

---
+++ PDScan.h
@@ -0,0 +1,32 @@
+/**************************************************************************/
+/* LabWindows/CVI User Interface Resource (UIR) Include File              */
+/* Copyright (c) National Instruments 2006. All Rights Reserved.          */
+/*                                                                        */
+/* WARNING: Do not add to, delete from, or otherwise modify the contents  */
+/*          of this include file.                                         */
+/**************************************************************************/
+
+#include <userint.h>
+
+#ifdef __cplusplus
+    extern "C" {
+#endif
+
+     /* Panels and Controls: */
+
+#define  PANEL                           1       /* callback function: panelCB */
+
+
+     /* Menu Bars, Menus, and Menu Items: */
+
+          /* (no menu bars in the resource file) */
+
+
+     /* Callback Prototypes: */ 
+
+int  CVICALLBACK panelCB(int panel, int event, void *callbackData, int eventData1, int eventData2);
+
+
+#ifdef __cplusplus
+    }
+#endif
