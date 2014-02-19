4ecaa9692ea193ef42bca5c445957e709e8e66dd
rothren <roth.rene@gmail.com>
2014-02-19 15:46:24 +0100

    Implement Program
    -get config from config.ini
    -scan a given folder for text files
    -filter those files given parameters
    -output everything in a table
    -output content of an INI file when clicking on a row in the table.

--- PDScan.c
+++ PDScan.c
@@ -16,6 +16,7 @@
 #include <userint.h>
 #include "PDScan.h"
 #include "toolbox.h"
+#include "inifile.h" 
 
 //==============================================================================
 // Constants
@@ -33,9 +34,17 @@ static int panelHandle = 0;
 
 //==============================================================================
 // Global variables
+char searchPath[512] = "C:\\ProzessDaten\\4-20mAP6200Zub\\*.ini";
+char pathName[512] =    "C:\\ProzessDaten\\4-20mAP6200Zub\\"; 
+char SECTION[128] = "Ergebnis";
+char KEY[128]	=	"Testresultat";
+char VALUE[128] =	"GES";
+
 
 //==============================================================================
 // Global functions
+
+int CVICALLBACK scan(void* functionData);
 
 /// HIFN The main entry-point function.
 int main (int argc, char *argv[])
@@ -45,6 +54,22 @@ int main (int argc, char *argv[])
 	/* initialize and load resources */
 	nullChk (InitCVIRTE (0, argv, 0));
 	errChk (panelHandle = LoadPanel (0, "PDScan.uir", PANEL));
+		
+	IniText iniConf;
+	iniConf = Ini_New(TRUE);
+	Ini_ReadFromFile(iniConf,"config.ini");
+	
+	Ini_GetStringIntoBuffer(iniConf,"MAIN","search",searchPath,512); 
+	Ini_GetStringIntoBuffer(iniConf,"MAIN","path",pathName,512); 
+	Ini_GetStringIntoBuffer(iniConf,"MAIN","section",SECTION,128); 
+	Ini_GetStringIntoBuffer(iniConf,"MAIN","key",KEY,128); 
+	Ini_GetStringIntoBuffer(iniConf,"MAIN","triggerValue",VALUE,128); 
+	
+	
+	Ini_Dispose(iniConf);
+							  
+	
+	
 	
 	/* display the panel and run the user interface */
 	errChk (DisplayPanel (panelHandle));
@@ -54,6 +79,64 @@ Error:
 	/* clean up */
 	if (panelHandle > 0)
 		DiscardPanel (panelHandle);
+	return 0;
+}
+
+
+int CVICALLBACK scan(void* functionData)
+{
+	int res=-1;
+	char fileName[MAX_FILENAME_LEN]="";
+	char path[MAX_PATHNAME_LEN] = "";
+	char date[15]="";
+	char time[15]="";
+	
+	DeleteTableRows(panelHandle,PANEL_TABLE,1,-1);
+	
+	
+	
+	res =  GetFirstFile(searchPath,1,1,0,0,0,0,fileName);
+	while(res == 0)
+	{
+		 IniText iniFile;
+		 char _value[255];
+		 int nrows = 0;
+		 iniFile = Ini_New(TRUE);
+		 MakePathname(pathName,fileName,path);
+		 Ini_ReadFromFile(iniFile,path);
+		 if((Ini_SectionExists(iniFile,SECTION)) && (Ini_ItemExists(iniFile,SECTION,KEY)))
+		 {
+		 
+			 Ini_GetStringIntoBuffer(iniFile,SECTION,KEY,_value,255);
+			 if (strncmp(_value,VALUE,3) == 0)
+			 {
+				 //fail!!!
+				 Ini_GetStringIntoBuffer(iniFile,"Pruefung","Datum",date,15);
+				 Ini_GetStringIntoBuffer(iniFile,"Pruefung","Zeit",time,15);
+				 InsertTableRows(panelHandle,PANEL_TABLE,-1,1,VAL_CELL_STRING);
+				 GetNumTableRows(panelHandle,PANEL_TABLE,&nrows);
+				 SetTableCellVal(panelHandle,PANEL_TABLE,MakePoint(1,nrows),fileName);
+				 SetTableCellVal(panelHandle,PANEL_TABLE,MakePoint(2,nrows),date);
+				 SetTableCellVal(panelHandle,PANEL_TABLE,MakePoint(3,nrows),time);
+				 
+				 
+			 
+			 
+			 }else{
+				//no fail
+
+			 
+			 }
+		 }
+		  Ini_Dispose(iniFile);
+
+		res = GetNextFile(fileName);
+	}
+	
+	
+	
+	SetCtrlAttribute(panelHandle,PANEL_COMMANDBUTTON,ATTR_LABEL_TEXT,"refresh list");
+	SetCtrlAttribute(panelHandle,PANEL_COMMANDBUTTON,ATTR_DIMMED,0);
 	return 0;
 }
 
@@ -89,6 +172,53 @@ int CVICALLBACK cbTABLE (int panel, int control, int event,
 		case EVENT_COMMIT:
 
 			break;
+		case EVENT_TABLE_ROW_COL_LABEL_CLICK:
+			if(eventData1 == 0)
+			{break;}
+			char iniToOpen[512]="";
+			char path[512]="";
+			GetTableCellVal(panelHandle,PANEL_TABLE,MakePoint(1,eventData1),iniToOpen);
+			MakePathname(pathName,iniToOpen,path);
+
+			 /**/
+				  FILE * pFile;
+				  long lSize;
+				  char * buffer;
+				  size_t result;
+
+				  pFile = fopen ( path , "rb" );
+				  if (pFile==NULL) { break;}
+
+				  // obtain file size:
+				  fseek (pFile , 0 , SEEK_END);
+				  lSize = (ftell (pFile)+1);
+				  rewind (pFile);
+
+				  // allocate memory to contain the whole file:
+				  buffer = (char*) malloc (sizeof(char)*lSize);
+				  if (buffer == NULL) {break;}
+
+				  // copy the file into the buffer:
+				  result = fread (buffer,1,(lSize-1),pFile);
+				  
+				  buffer[lSize-1]='\0';
+				  if (result != (lSize-1)) {break;}
+
+				  /* the whole file is now loaded in the memory buffer. */
+
+				  // terminate
+				  
+				 
+				  
+				  
+				 /**/
+			ResetTextBox(panelHandle,PANEL_TEXTBOX,buffer);
+			fclose (pFile); 
+			 free (buffer);
+
+			
+			
+			break;
 	}
 	return 0;
 }
@@ -99,7 +229,10 @@ int CVICALLBACK cbRefresh (int panel, int control, int event,
 	switch (event)
 	{
 		case EVENT_COMMIT:
-
+			SetCtrlAttribute(panelHandle,PANEL_COMMANDBUTTON,ATTR_LABEL_TEXT,"refreshing list...");
+			SetCtrlAttribute(panelHandle,PANEL_COMMANDBUTTON,ATTR_DIMMED,1);
+			CmtThreadFunctionID functionID;
+			CmtScheduleThreadPoolFunction(DEFAULT_THREAD_POOL_HANDLE,scan,(void*)NULL,&functionID);
 			break;
 	}
 	return 0;
